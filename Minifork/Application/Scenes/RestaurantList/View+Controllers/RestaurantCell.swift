36d1e76dc8adf466589cc222e428bbe257024b91
Felipe Fernandes <felipe@govegans.it>
2021-12-18 21:30:25 +0100

    added assets + start to layout views in restaurant cell

--- Minifork/Application/Scenes/RestaurantList/View+Controllers/RestaurantCell.swift
+++ Minifork/Application/Scenes/RestaurantList/View+Controllers/RestaurantCell.swift
@@ -14,10 +14,10 @@ class RestaurantViewCell: UITableViewCell {
 
   static let identifier = "restaurantEntity"
 
-  // MARK: VIEW
+  // MARK: VIEWS
   lazy var background: UIView = {
     let view = UIView()
-    view.backgroundColor = .blue
+    view.backgroundColor = .red
     view.layer.cornerRadius = 20
     return view
   }()
@@ -35,16 +35,33 @@ class RestaurantViewCell: UITableViewCell {
     return view
   }()
 
+  lazy var bannerOne: UIView = {
+    let view = UIView()
+    view.backgroundColor = .brown
+    return view
+  }()
+
+  lazy var bannerTwo: UIView = {
+    let view = UIView()
+    view.backgroundColor = .blue
+    return view
+  }()
+
   // MARK: LABELS
   lazy var restaurantName: UILabel = {
     let label = UILabel()
     label.textColor = .red
+    label.lineBreakMode = .byClipping
+    label.numberOfLines = 0
+    label.textAlignment = .center
     return label
   }()
 
   lazy var restaurantCousine: UILabel = {
     let label = UILabel()
     label.textColor = .blue
+    label.numberOfLines = 1
+    label.lineBreakMode = .byClipping
     return label
   }()
 
@@ -52,57 +69,81 @@ class RestaurantViewCell: UITableViewCell {
     let label = UILabel()
     label.textColor = .blue
     return label
-  }
+  }()
+
+  lazy var theForkReview: UILabel = {
+    let label = UILabel()
+    label.textColor = .cyan
+    return label
+  }()
+
+  lazy var priceRange: UILabel = {
+    let label = UILabel()
+    label.textColor = .cyan
+    return label
+  }()
+
+  lazy var addressStreet: UILabel = {
+    let label = UILabel()
+    label.lineBreakMode = .byClipping
+    label.numberOfLines = 0
+    label.textAlignment = .left
+    return label
+  }()
+
+  lazy var addressCityCountry: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+
+  lazy var offerLabel: UILabel = {
+    let label = UILabel()
+    return label
+  }()
 
 
   // MARK: ICONS
   lazy var cousineIcon: UIImageView = {
     let view = UIImageView()
-    backgroundColor = .cyan
+    view.image = UIImage(named: "food")
     return view
   }()
 
   lazy var tripAdviserIcon: UIImageView = {
     let view = UIImageView()
-    backgroundColor = .cyan
+    view.image = UIImage(named: "ta-logo")
     return view
   }()
 
   lazy var theForkIcon: UIImageView = {
     let view = UIImageView()
-    backgroundColor = .cyan
+    view.image = UIImage(named: "tf-logo")
     return view
   }()
 
   lazy var priceRangeIcon: UIImageView = {
     let view = UIImageView()
-    backgroundColor = .cyan
+    view.image = UIImage(named: "cash")
     return view
   }()
 
   lazy var locationIcon: UIImageView = {
     let view = UIImageView()
-    backgroundColor = .cyan
+    view.image = UIImage(named: "location")
     return view
   }()
 
   lazy var shareIcon: UIImageView = {
     let view = UIImageView()
-    backgroundColor = .cyan
+    view.image = UIImage(named: "share")
     return view
   }()
 
   lazy var favIcon: UIImageView = {
     let view = UIImageView()
-    backgroundColor = .cyan
+    view.image = UIImage(named: "empty-heart")
     return view
   }()
-
-
-
-
-
-
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -115,7 +156,43 @@ class RestaurantViewCell: UITableViewCell {
 
 
   func configure() {
-    self.contentView.addSubview(background)
+    contentView.addSubview(background)
+    background.addSubview(backgroundPicture)
+    backgroundPicture.addSubview(overlay)
+    background.addSubview(tripAdviserIcon)
+    background.addSubview(tripReview)
+    background.addSubview(theForkIcon)
+    background.addSubview(theForkReview)
+    background.addSubview(priceRangeIcon)
+    background.addSubview(priceRange)
+    background.addSubview(bannerOne)
+    background.addSubview(bannerTwo)
+
+    overlay.addSubview(restaurantName)
+    overlay.addSubview(restaurantCousine)
+    overlay.addSubview(cousineIcon)
+
+    bannerOne.addSubview(locationIcon)
+    bannerOne.addSubview(addressStreet)
+    bannerOne.addSubview(addressCityCountry)
+    bannerOne.addSubview(shareIcon)
+    bannerOne.addSubview(favIcon)
+
+    bannerTwo.addSubview(offerLabel)
+
+
+    self.backgroundColor = .clear
+    // TEST
+
+    restaurantName.text = "Babobba aksd kamksdm amksldmaks"
+    restaurantCousine.text = "French"
+
+    tripReview.text = "2.3"
+    theForkReview.text = "44.3"
+    priceRange.text = "0-22€"
+    addressStreet.text = "3huiANSJKDNASJKDNJASKNDASDNSA"
+    addressCityCountry.text = "Salvador, Brasil"
+    offerLabel.text = "50% sconto alla casssa , sticazzi toma toma toma toma"
   }
 
   func bind(viewModel: Any) {
@@ -133,10 +210,87 @@ class RestaurantViewCell: UITableViewCell {
   override func layoutSubviews() {
     super.layoutSubviews()
 
-    background.pin
-      .horizontally(20)
-      .height(50)
+    background.pin.horizontally(20)
+    backgroundPicture.pin.horizontally()
+    overlay.pin.horizontally(40)
 
+    restaurantName.pin.horizontally(20)
+    restaurantName.pin.sizeToFit(.width)
+
+    restaurantCousine.pin
+      .below(of: restaurantName)
+      .marginTop(10)
+      .sizeToFit()
+      .hCenter(10)
+
+    cousineIcon.pin
+      .left(of: restaurantCousine)
+      .marginRight(5)
+      .width(15)
+      .height(20)
+      .vCenter(to: restaurantCousine.edge.vCenter)
+
+
+    overlay.pin.wrapContent(.vertically, padding: 10)
+    backgroundPicture.pin.wrapContent(.vertically, padding: 25)
+
+
+    tripAdviserIcon.pin
+      .below(of: backgroundPicture)
+      .width(30)
+      .height(15)
+      .marginTop(10)
+      .left(10)
+
+    tripReview.pin
+      .right(of: tripAdviserIcon)
+      .sizeToFit()
+      .marginLeft(8)
+      .vCenter(to: tripAdviserIcon.edge.vCenter)
+
+    theForkIcon.pin
+      .right(of: tripReview)
+      .marginLeft(10)
+      .width(15)
+      .height(20)
+      .vCenter(to: tripAdviserIcon.edge.vCenter)
+
+    theForkReview.pin
+      .right(of: theForkIcon)
+      .sizeToFit()
+      .marginLeft(8)
+      .vCenter(to: theForkIcon.edge.vCenter)
+
+    priceRange.pin
+      .sizeToFit()
+      .right(10)
+      .vCenter(to: tripAdviserIcon.edge.vCenter)
+
+    priceRangeIcon.pin
+      .left(of: priceRange)
+      .marginRight(9)
+      .width(20)
+      .height(15)
+      .vCenter(to: tripAdviserIcon.edge.vCenter)
+
+
+
+    bannerOne.pin.horizontally(20)
+
+    locationIcon.pin
+      .height(30)
+      .width(20)
+      .left()
+
+    addressStreet.pin
+      .right(40%)
+      .right(of: locationIcon)
+      .marginLeft(5)
+      .sizeToFit(.width)
+      .top(to: locationIcon.edge.top)
+
+
+    self.background.pin.wrapContent(.vertically)
     self.contentView.pin.wrapContent(.vertically, padding: 10)
   }
 
